46035c80bf4b1346338ae27a66e2af841fca41a5
Vergur <elovikovgri@yandex.ru>
2024-04-22 16:02:34 +0300

    feat: add menu scenes controller; fix: add images atlas

---
+++ Assets/Scripts/MenuSceneController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MenuSceneController : MonoBehaviour
+{
+    [Header("Screens")] 
+    [SerializeField] private GameObject _settingsScreen;
+    [SerializeField] private GameObject _mainScreen;
+
+    public void ShowSettingsScreen()
+    {
+        _settingsScreen.SetActive(true);
+        _mainScreen.SetActive(false);
+    }
+    
+    public void ShowMainScreen()
+    {
+        _settingsScreen.SetActive(false);
+        _mainScreen.SetActive(true);
+    }
+}
